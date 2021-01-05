76b289155ca24c051e3a50d3971f16485d53b739
jenra <xiwobx@gmail.com>
2021-01-04 20:08:35 -0500

    Added comments and parser.slice()

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -1,8 +1,10 @@
 use logos::{Logos, Lexer, Span};
 
+// The tokens parsed by the lexer.
 #[derive(Logos, PartialEq, Debug, Copy, Clone)]
 pub enum Token
 {
+    // Brackets
     #[token("(")]
     LParen,
    
@@ -20,16 +22,19 @@ pub enum Token
    
     #[token("}")]
     RBrace,
-   
+  
+    // Whitespace
     #[token("\n")]
     Newline,
    
     #[regex(r"[ \t\f]+", logos::skip)]
     Whitespace,
-   
+
+    // Error
     #[error]
     Error,
 
+    // Punctuation and symbols
     #[token(":")]
     Colon,
     
@@ -72,6 +77,7 @@ pub enum Token
     #[token("^")]
     Caret,
     
+    // Numbers
     #[regex(r"[0-9]+", |lex| lex.slice().parse())]
     #[regex(r"0x[0-9a-fA-F]+", |lex| i64::from_str_radix(&lex.slice()[2..], 16))]
     #[regex(r"0b[01]+", |lex| i64::from_str_radix(&lex.slice()[2..], 2))]
@@ -80,21 +86,26 @@ pub enum Token
     #[regex(r"[0-9]+(\.[0-9]*([eE][+-]?[0-9]+)?|[eE][+-]?[0-9]+)", |lex| lex.slice().parse())]
     Float(f64),
     
+    // Symbols (variables and stuff)
     #[regex(r"[a-zA-Z_\$][a-zA-Z0-9_']")]
     Symbol,
     
+    // Strings
     #[regex(r#""([^\\]|\\.)*""#)]
     String,
     
+    // Booleans
     #[regex("true|false")]
     Bool,
     
+    // Arrows
     #[token("->")]
     RightArrow,
     
     #[token("=>")]
     ThiccArrow,
     
+    // Keywords
     #[token("with")]
     With,
     
@@ -150,15 +161,23 @@ pub enum Token
     Xor,
 }
 
+// Represents a parser.
 struct Parser<'a>
 {
+    // The lexer the parser uses internally.
     lexer: Lexer<'a, Token>,
+
+    // The tokens already parsed
     tokens: Vec<(Token, Span)>,
+
+    // The current position of the parser.
     token_pos: usize
 }
 
 impl<'a> Parser<'a>
 {
+    // new(&str) -> Parser
+    // Creates a new parser
     fn new(s: &str) -> Parser
     {
         Parser {
@@ -168,29 +187,37 @@ impl<'a> Parser<'a>
         }
     }
 
+    // next(&mut self) -> Option<&(Token, Span)>
+    // Gets the next token.
     fn next<'b>(&'b mut self) -> Option<&'b (Token, Span)>
     {
+        // Get token from list of already parsed tokens if it exists
         if self.token_pos < self.tokens.len()
         {
             let token = &self.tokens[self.token_pos];
             self.token_pos += 1;
             Some(token)
+
+        // Otherwise get token from the lexer
         } else
         {
             let span = self.lexer.span();
-            
             self.tokens.push((self.lexer.next()?, span));
             self.tokens.last()
         }
     }
 
-
+    // peek(&mut self) -> Option<&(Token, Span)>
+    // Peeks at the next token.
     fn peek<'b>(&'b mut self) -> Option<&'b (Token, Span)>
     {
+        // Get token from list of already parsed tokens if it exists
         if self.token_pos < self.tokens.len()
         {
             let token = &self.tokens[self.token_pos];
             Some(token)
+
+        // Otherwise get token from lexer
         } else
         {
             let span = self.lexer.span();
@@ -199,11 +226,29 @@ impl<'a> Parser<'a>
         }
     }
 
+    // slice(&self) -> String
+    // Returns the slice corresponding to the current token.
+    fn slice(&self) -> String
+    {
+        if self.token_pos < self.tokens.len()
+        {
+            let range = &self.tokens[self.token_pos].1;
+            String::from(&self.lexer.source()[range.start..range.end])
+        } else
+        {
+            String::from(self.lexer.slice())
+        }
+    }
+
+    // save_state(&self) -> usize
+    // Saves the current token position by returning it.
     fn save_state(&self) -> usize
     {
         self.token_pos
     }
 
+    // return_state(&mut self, usize) -> ()
+    // Returns to a given state.
     fn return_state(&mut self, state: usize)
     {
         self.token_pos = state;
@@ -213,10 +258,17 @@ impl<'a> Parser<'a>
 #[derive(Debug, PartialEq)]
 pub enum AST
 {
+    // Numbers
     Int(i64),
     Float(f64),
+
+    // String
     String(String),
+
+    // Symbol (variables and stuff)
     Symbol(String),
+
+    // Infix expressions
     Infix(String, Box<AST>, Box<AST>)
 }
 
@@ -224,26 +276,36 @@ pub enum AST
 // Gets the next value.
 fn value(parser: &mut Parser) -> Option<AST>
 {
+    // Get token
     let token = parser.peek()?.0;
 
+    // Check for int
     if let Token::Int(n) = token
     {
         parser.next();
         Some(AST::Int(n))
+
+    // Check for float
     } else if let Token::Float(n) = token
     {
         parser.next();
         Some(AST::Float(n))
+
+    // Check for string
     } else if let Token::String = token
     {
-        let s = parser.lexer.slice();
+        let s = parser.slice();
         parser.next();
         Some(AST::String(String::from(s)))
+
+    // Check for symbol
     } else if let Token::Symbol = token
     {
-        let s = parser.lexer.slice();
+        let s = parser.slice();
         parser.next();
         Some(AST::Symbol(String::from(s)))
+
+    // Not a value
     } else
     {
         None
@@ -254,20 +316,23 @@ fn value(parser: &mut Parser) -> Option<AST>
 // Gets the next multiplication/division/modulus expression.
 fn muldivmod(parser: &mut Parser) -> Option<AST>
 {
-
+    // Set up
     let mut left = value(parser)?;
     let state = parser.save_state();
     
     loop
     {
+        // Save current state
         let mut state2 = parser.save_state();
 
+        // Check for operator
         if let Some(op) = parser.peek()
         {
+            // Get operator
             let op = match op.0
             {
                 Token::Mul => String::from("*"),
-                Token::DivMod => String::from(parser.lexer.slice()),
+                Token::DivMod => String::from(parser.slice()),
                 _ => {
                     parser.return_state(state2);
                     break;
@@ -275,6 +340,7 @@ fn muldivmod(parser: &mut Parser) -> Option<AST>
             };
             parser.next();
 
+            // Get right hand side
             let right = match value(parser)
             {
                 Some(v) => v,
@@ -283,7 +349,11 @@ fn muldivmod(parser: &mut Parser) -> Option<AST>
                     return None;
                 }
             };
+
+            // Build ast
             left = AST::Infix(op, Box::new(left), Box::new(right));
+        
+        // If there's no operator, break
         } else
         {
             break;
