b7de6d6a6cf8dcf0f1badc09b376d1fe7a62a3d5
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-01-20 21:19:40 -0500

    Added sum types

--- src/frontend/types.rs
+++ src/frontend/types.rs
@@ -14,6 +14,8 @@ pub enum Type
     Bool,
     String,
     Func(Box<Type>, Box<Type>),
+    Sum(Vec<Type>),
+    Enum(String)
 }
 
 // convert_ast_to_type(AST) -> Type
@@ -22,20 +24,58 @@ pub fn convert_ast_to_type(ast: AST) -> Type
 {
     match ast
     {
+        // Symbols
         AST::Symbol(s, v) => {
             match v.as_str()
             {
+                // Primatives
                 "Int" => Type::Int,
                 "Float" => Type::Float,
                 "Bool" => Type::Bool,
+
+                // Error
                 _ => Type::ConversionError(s)
             }
         }
 
-        AST::Infix(_, op, l, r) if op.as_str() == "->" =>
+        // Enums
+        AST::Prefix(_, op, v) if op == "enum" =>
+            if let AST::Symbol(_, v) = *v
+            {
+                Type::Enum(v)
+            } else
+            {
+                unreachable!("enum always has a symbol");
+            }
+
+        // Sum types
+        AST::Infix(_, op, l, r) if op == "|" => {
+            let mut fields = vec![convert_ast_to_type(*r)];
+            let mut acc = *l;
+
+            loop 
+            {
+                match acc
+                {
+                    AST::Infix(_, op, l, r) if op == "|" => {
+                        fields.push(convert_ast_to_type(*r));
+                        acc = *l;
+                    }
+
+                    _ => break
+                }
+            }
+
+            fields.push(convert_ast_to_type(acc));
+            Type::Sum(fields)
+        }
+
+        // Function types
+        AST::Infix(_, op, l, r) if op == "->" =>
             Type::Func(Box::new(convert_ast_to_type(*l)), Box::new(convert_ast_to_type(*r))),
 
-        AST::Prefix(_, op, v) if op.as_str() == "" =>
+        // Parenthesised types
+        AST::Prefix(_, op, v) if op == "" =>
             convert_ast_to_type(*v),
 
         _ => Type::ConversionError(ast.get_span())
